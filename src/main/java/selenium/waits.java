f26823a7a3a67f2fce8782b386d587c450e9f192
Keerthivasan <nkeerthivasan04@gmail.com>
2024-08-07 07:33:14 +0530

    waits update

--- src/main/java/selenium/waits.java
+++ src/main/java/selenium/waits.java
@@ -3,9 +3,13 @@ package selenium;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class waits {
@@ -17,17 +21,25 @@ public class waits {
 		FirefoxDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("https://letCode.in");
-		
-		//implicit wait
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		//Explicit wait
-		WebElement ele = driver.findElement(By.id("abd"));
-		WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-		webDriverWait.until(ExpectedConditions.elementToBeClickable(ele));
-		//fluent wait
-		
-//		Wait wait = new FluentWait(driver).withTimeout(5, TimeUnit.SECONDS).pollingEvery(5, TimeUnit.SECONDS).ignoring(Exception.class);
-	}
+		WebElement submit = driver.findElement(By.id("btnSubmit"));
+		submit.click();
+
+		// screenshot
+		driver.getScreenshotAs(OutputType.FILE);	//or
+		((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+		//implicit wait -- waits for each and every action performed in the UI
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+
+		//explicit wait -- waits until the expected condition is getting success/true
+		WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+		webDriverWait.until(ExpectedConditions.elementToBeClickable(submit));
+
+		//Fluent wait -- waits in an interval of time and again performs the same action (polling period)
+		Wait wait = new FluentWait(driver)
+				.withTimeout(5, TimeUnit.SECONDS)
+				.pollingEvery(3, TimeUnit.SECONDS)
+				.ignoring(Exception.class);
+		}
 
 }
